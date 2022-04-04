cb7204e37bdbf17b08eb04f910c73ba4906ff87a
Eridanus Sora <sora@sound.moe>
2022-04-04 14:18:02 +0800

    reduce duplicated requests

--- src/components/LoginStatus/index.tsx
+++ src/components/LoginStatus/index.tsx
@@ -1,42 +1,26 @@
 import React, { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import useMessage from "@/hooks/useMessage";
-import { CurrentUserInfo, IsLoadedExtendUserInfo, IsLoadingUserInfo } from "@/state/user";
+import { CurrentUserInfo, IsLoadingUserInfo } from "@/state/user";
 import { CredentialState } from "@/state/credentials";
-import { FavoriteTracksState } from "@/state/favorite";
-import { PlaylistsState } from "@/state/playlists";
 import { AnniwBusinessError } from "@/api/request";
-import {
-    getAvailableAnnilTokens,
-    getFavoriteTracks,
-    getUserInfo,
-    getFavoritePlaylists,
-} from "./services";
+import { getAvailableAnnilTokens, getUserInfo } from "./services";
 
 const LoginStatus: React.FC = () => {
     const setIsLoadingUserInfo = useSetRecoilState(IsLoadingUserInfo);
     const setCurrentUserInfo = useSetRecoilState(CurrentUserInfo);
     const setCredential = useSetRecoilState(CredentialState);
-    const setFavoriteTracks = useSetRecoilState(FavoriteTracksState);
-    const setFavoritePlaylists = useSetRecoilState(PlaylistsState);
-    const setIsLoadedExtendUserInfo = useSetRecoilState(IsLoadedExtendUserInfo);
     const [_, { addMessage }] = useMessage();
     useEffect(() => {
         (async () => {
             setIsLoadingUserInfo(true);
             try {
-                const [userInfo, availableTokens, favoriteTracks, favoritePlaylists] =
-                    await Promise.all([
-                        getUserInfo(),
-                        getAvailableAnnilTokens(),
-                        getFavoriteTracks(),
-                        getFavoritePlaylists(),
-                    ]);
+                const [userInfo, availableTokens] = await Promise.all([
+                    getUserInfo(),
+                    getAvailableAnnilTokens(),
+                ]);
                 setCurrentUserInfo(userInfo);
                 setCredential({ credentials: availableTokens });
-                setFavoriteTracks(favoriteTracks);
-                setFavoritePlaylists(favoritePlaylists);
-                setIsLoadedExtendUserInfo(true);
             } catch (e) {
                 if (e instanceof AnniwBusinessError && e.code === 902002) {
                     // 未登录 忽略
@@ -47,15 +31,7 @@ const LoginStatus: React.FC = () => {
                 setIsLoadingUserInfo(false);
             }
         })();
-    }, [
-        addMessage,
-        setCredential,
-        setCurrentUserInfo,
-        setFavoritePlaylists,
-        setFavoriteTracks,
-        setIsLoadingUserInfo,
-        setIsLoadedExtendUserInfo,
-    ]);
+    }, [addMessage, setCredential, setCurrentUserInfo, setIsLoadingUserInfo]);
     return null;
 };
 
