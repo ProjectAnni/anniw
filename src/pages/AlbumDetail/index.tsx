a64b94db6fb7abb3016f6f8f748dcbe74b8b6921
Yesterday17 <mmf@mmf.moe>
2022-04-14 17:16:26 +0800

    do not cache in album detail page

--- src/pages/AlbumDetail/index.tsx
+++ src/pages/AlbumDetail/index.tsx
@@ -16,7 +16,7 @@ import TrackList, { TrackListImperativeHandles } from "@/components/TrackList";
 import { TrackItem, TrackItemType, TrackListFeatures } from "@/components/TrackList/types";
 import AlbumCover from "./components/AlbumCover";
 import AlbumBasicInfo from "./components/AlbumBasicInfo";
-import { getAlbumInfo } from "../../components/AlbumWall/services";
+import { getAlbumInfo } from "./services";
 import styles from "./index.module.scss";
 import { getAvailableLibraryForTrack } from "@/utils/library";
 
--- src/pages/AlbumDetail/services.ts
+++ src/pages/AlbumDetail/services.ts
@@ -1,4 +1,30 @@
+import request from "@/api/request";
 import { default as AlbumDB } from "@/db/album";
+import { formatResponse } from "@/utils/format";
+import { AlbumDetail } from "@/types/common";
+
+// AlbumDetail loads album metadata forcefully from server
+export async function getAlbumInfo(albumId: string | null) {
+    if (!albumId) {
+        return;
+    }
+    const albumInfoResponse = await request.get<Record<string, AlbumDetail | null>>(
+        `/api/meta/album`,
+        {
+            id: [albumId],
+        },
+        {
+            formatResponse: false,
+        }
+    );
+    const albumInfo = formatResponse(albumInfoResponse?.[albumId]) as AlbumDetail;
+    if (albumInfo) {
+        // save to database
+        await AlbumDB.addAlbumInfo(albumInfo);
+        // get InheritedAlbumDetail
+        return await AlbumDB.getAlbumInfo(albumId);
+    }
+}
 
 export async function getAlbumAvailableLibraries(albumId: string | null) {
     if (!albumId) {
