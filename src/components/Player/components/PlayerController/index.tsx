5341ca21c0ca9a50703e0ba883806c58ff1a31b8
snylonue <snylonue@gmail.com>
2022-05-05 17:08:20 +0800

    add playPrev

--- src/components/Player/components/PlayerController/index.tsx
+++ src/components/Player/components/PlayerController/index.tsx
@@ -4,18 +4,20 @@ import { Grid, CircularProgress } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import PlayIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
+import PrevIcon from "@mui/icons-material/SkipPrevious"
 import NextIcon from "@mui/icons-material/SkipNext";
 import usePlayer from "@/hooks/usePlayer";
 import { PlayerStatusState } from "@/state/player";
 import { PlayerStatus } from "@/types/common";
 
 interface Props {
+    playPrev: () => void;
     playNext: () => void;
     playFirst: () => void;
 }
 
 const PlayerController: React.FC<Props> = (props) => {
-    const { playNext, playFirst } = props;
+    const { playPrev, playNext, playFirst } = props;
     const playerStatus = useRecoilValue(PlayerStatusState);
     const [player, { resume, pause, restart }] = usePlayer();
     return (
@@ -44,6 +46,9 @@ const PlayerController: React.FC<Props> = (props) => {
                         <CircularProgress color="inherit" size="35px" />
                     )}
                 </>
+            </IconButton>
+            <IconButton color="inherit" aria-label="menu" onClick={playPrev}>
+                <PrevIcon />
             </IconButton>
             <IconButton color="inherit" aria-label="menu" onClick={playNext}>
                 <NextIcon />
