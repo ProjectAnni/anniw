78d33e5d80094b36e96460353e42507d2558b9d8
SeraphJACK <seraphjack@outlook.com>
2022-07-24 15:55:33 +0800

    Share cover & info

--- src/pages/ShareDetail/index.tsx
+++ src/pages/ShareDetail/index.tsx
@@ -3,10 +3,11 @@ import { useParams } from "react-router";
 import useMessage from "@/hooks/useMessage";
 import { ExportedPlaylist } from "@/types/exported";
 import { getShare } from "@/pages/ShareDetail/services";
-import { parsePlaylists } from "@/pages/ShareDetail/util";
+import { getCoverUrl, parsePlaylists } from "@/pages/ShareDetail/util";
 import { PlayQueueItem } from "@/types/common";
-import { Grid, Typography } from "@mui/material";
+import { Divider, Grid, List, Typography } from "@mui/material";
 import styles from "./index.module.scss";
+import Cover from "@/components/Cover";
 
 const ShareDetail = () => {
     const { shareId } = useParams<{ shareId: string | undefined }>();
@@ -42,15 +43,43 @@ const ShareDetail = () => {
     }, [share]);
 
     return (
-      <Grid container justifyContent="center">
-          <Grid container flexDirection="column" className={styles.pageContainer}>
-              <div>
-                  <Typography variant="h4" className={styles.name}>
-                      <span title={share?.name}>{share?.name}</span>
-                  </Typography>
-              </div>
-          </Grid>
-      </Grid>
+        share ?
+            <Grid container justifyContent="center" className={styles.pageContainer}>
+                <Grid item xs={12} lg={8}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} lg={3}>
+                            <div className={styles.coverContainer}>
+                                <Cover coverUrl={getCoverUrl(share)} />
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} lg={9} className={styles.titleContainer}>
+                            <div>
+                                <Typography variant="h4" className={styles.name}>
+                                    <span title={share.name}>{share.name}</span>
+                                </Typography>
+                            </div>
+                            <div className={styles.description}>
+                                {!!share.description &&
+                                  <Typography variant="subtitle1">{share.description}</Typography>}
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} lg={8} className={styles.divider}>
+                    <Divider />
+                </Grid>
+                <Grid item xs={12} lg={8}>
+                    <List dense>
+
+                    </List>
+                </Grid>
+            </Grid>
+            :
+            <>
+                <Typography variant="h2">
+                    加载中
+                </Typography>
+            </>
     );
 };
 
