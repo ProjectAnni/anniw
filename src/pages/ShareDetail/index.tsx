e53e8b0ef9650b56866213f05d4a37c066cfc565
Eridanus Sora <sora@sound.moe>
2022-08-21 22:14:33 +0800

    fix share page styles

--- src/pages/ShareDetail/index.tsx
+++ src/pages/ShareDetail/index.tsx
@@ -24,6 +24,7 @@ import usePlayQueueController from "@/hooks/usePlayQueueController";
 import { useRecoilValue } from "recoil";
 import { NowPlayingInfoState, PlayerStatusState } from "@/state/player";
 import usePlayer from "@/hooks/usePlayer";
+import Artist from "@/components/Artist";
 
 const TypeTextMap: Record<string, string> = {
     instrumental: "伴奏",
@@ -76,11 +77,16 @@ const ShareDetail = () => {
         trackId: nowPlayingTrackId,
     } = useRecoilValue(NowPlayingInfoState);
 
-    const isPlaying = useCallback((idx: number) => {
-        return nowPlayingAlbumId === tracks[idx].albumId
-            && nowPlayingDiscId === tracks[idx].discId
-            && nowPlayingTrackId === tracks[idx].trackId;
-    }, [tracks, nowPlayingAlbumId, nowPlayingDiscId, nowPlayingTrackId]);
+    const isPlaying = useCallback(
+        (idx: number) => {
+            return (
+                nowPlayingAlbumId === tracks[idx].albumId &&
+                nowPlayingDiscId === tracks[idx].discId &&
+                nowPlayingTrackId === tracks[idx].trackId
+            );
+        },
+        [tracks, nowPlayingAlbumId, nowPlayingDiscId, nowPlayingTrackId]
+    );
 
     const handlePlay = (idx: number) => {
         if (isPlaying(idx)) {
@@ -96,25 +102,97 @@ const ShareDetail = () => {
         }
     };
 
-    return (
-        share ?
+    return share ? (
+        <Grid container justifyContent="center" className={styles.pageContainer}>
+            <Grid item xs={12} lg={8}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} lg={3}>
+                        <div className={styles.coverContainer}>
+                            <Cover coverUrl={getCoverUrl(share)} />
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} lg={9} flexDirection="column">
+                        <div>
+                            <Typography variant="h4" className={styles.name}>
+                                <span title={share.name}>{share.name}</span>
+                            </Typography>
+                        </div>
+                        <div className={styles.description}>
+                            {!!share.description && (
+                                <Typography variant="subtitle1">{share.description}</Typography>
+                            )}
+                        </div>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} lg={8} className={styles.divider}>
+                <Divider />
+            </Grid>
+            <Grid item xs={12} lg={8}>
+                <List dense>
+                    {tracks.map((track, idx) => {
+                        return (
+                            <ListItem
+                                className={classNames({
+                                    [styles.oddItem]: idx % 2 === 0,
+                                })}
+                                key={shareId + "-" + idx}
+                            >
+                                <ListItemIcon>
+                                    <IconButton onClick={() => handlePlay(idx)}>
+                                        {isPlaying(idx) ? (
+                                            playerStatus === PlayerStatus.PAUSED ||
+                                            playerStatus === PlayerStatus.ENDED ||
+                                            playerStatus === PlayerStatus.EMPTY ||
+                                            playerStatus === PlayerStatus.BUFFERING ? (
+                                                <PlayArrow />
+                                            ) : (
+                                                <Pause />
+                                            )
+                                        ) : (
+                                            <PlayArrow />
+                                        )}
+                                    </IconButton>
+                                </ListItemIcon>
+                                <ListItemText
+                                    disableTypography
+                                    primary={
+                                        <div className={styles.trackTitleContainer}>
+                                            <div className={styles.title}>
+                                                <span>
+                                                    {`${(idx + 1).toString().padStart(2, "0")}.`}
+                                                    &nbsp;
+                                                </span>
+                                                <span title={track.title}>{track.title}</span>
+                                            </div>
+                                            {track.type !== "normal" && (
+                                                <div className={styles.tag}>
+                                                    {TypeTextMap[track.type]}
+                                                </div>
+                                            )}
+                                        </div>
+                                    }
+                                    secondary={
+                                        <div className={styles.artist}>
+                                            <Artist artist={track.artist} />
+                                        </div>
+                                    }
+                                />
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </Grid>
+        </Grid>
+    ) : (
+        <>
             <Grid container justifyContent="center" className={styles.pageContainer}>
+                <div style={{ marginTop: "8px" }}></div>
                 <Grid item xs={12} lg={8}>
                     <Grid container spacing={2}>
                         <Grid item xs={12} lg={3}>
                             <div className={styles.coverContainer}>
-                                <Cover coverUrl={getCoverUrl(share)} />
-                            </div>
-                        </Grid>
-                        <Grid item xs={12} lg={9} className={styles.titleContainer}>
-                            <div>
-                                <Typography variant="h4" className={styles.name}>
-                                    <span title={share.name}>{share.name}</span>
-                                </Typography>
-                            </div>
-                            <div className={styles.description}>
-                                {!!share.description &&
-                                  <Typography variant="subtitle1">{share.description}</Typography>}
+                                <Skeleton variant="rectangular" width="100%" height="100%" />
                             </div>
                         </Grid>
                     </Grid>
@@ -123,87 +201,19 @@ const ShareDetail = () => {
                     <Divider />
                 </Grid>
                 <Grid item xs={12} lg={8}>
-                    <List dense>
-                        {tracks.map((track, idx) => {
-                            return (
-                                <ListItem
-                                    className={classNames({
-                                        [styles.oddItem]: idx % 2 === 0,
-                                    })}
-                                    key={shareId + "-" + idx}>
-                                    <ListItemIcon>
-                                        <IconButton onClick={() => handlePlay(idx)}>
-                                            {isPlaying(idx) ? (
-                                                playerStatus === PlayerStatus.PAUSED ||
-                                                playerStatus === PlayerStatus.ENDED ||
-                                                playerStatus === PlayerStatus.EMPTY ||
-                                                playerStatus === PlayerStatus.BUFFERING ? (
-                                                    <PlayArrow />
-                                                ) : (
-                                                    <Pause />
-                                                )
-                                            ) : (
-                                                <PlayArrow />
-                                            )}
-                                        </IconButton>
-                                    </ListItemIcon>
-                                    <ListItemText
-                                        primary={
-                                            <div className={styles.titleContainer}>
-                                                <div className={styles.title}>
-                                                    <span>
-                                                        {`${(idx + 1).toString().padStart(2, "0")}.`}&nbsp;
-                                                    </span>
-                                                    <span title={track.title}>{track.title}</span>
-                                                </div>
-                                                {track.type !== "normal" && (
-                                                    <div className={styles.tag}>{TypeTextMap[track.type]}</div>
-                                                )}
-                                            </div>
-                                        }
-                                        secondary={
-                                            <div>
-                                                {track.artist}
-                                            </div>
-                                        }
-                                    />
-                                </ListItem>
-                            );
-                        })}
-                    </List>
+                    {new Array(10).fill(0).map((_, index) => (
+                        <Skeleton
+                            variant="rectangular"
+                            height={62}
+                            sx={{
+                                bgcolor: index % 2 === 0 ? "rgba(238, 238, 238, 0.2)" : "#fff",
+                            }}
+                            key={index}
+                        />
+                    ))}
                 </Grid>
             </Grid>
-            :
-            <>
-                <Grid container justifyContent="center" className={styles.pageContainer}>
-                    <div style={{ marginTop: "8px" }}></div>
-                    <Grid item xs={12} lg={8}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} lg={3}>
-                                <div className={styles.coverContainer}>
-                                    <Skeleton variant="rectangular" width="100%" height="100%" />
-                                </div>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} lg={8} className={styles.divider}>
-                        <Divider />
-                    </Grid>
-                    <Grid item xs={12} lg={8}>
-                        {new Array(10).fill(0).map((_, index) => (
-                            <Skeleton
-                                variant="rectangular"
-                                height={62}
-                                sx={{
-                                    bgcolor:
-                                        index % 2 === 0 ? "rgba(238, 238, 238, 0.2)" : "#fff",
-                                }}
-                                key={index}
-                            />
-                        ))}
-                    </Grid>
-                </Grid>
-            </>
+        </>
     );
 };
 
