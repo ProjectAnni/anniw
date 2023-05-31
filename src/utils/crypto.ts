08f60a7789120f697ae019ab9038444085f99230
Eridanus Sora <sora@sound.moe>
2023-05-31 23:09:29 +0800

    lazy load crypto-js

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,13 +1,10 @@
-import sha256_cryptojs from "crypto-js/sha256";
-
-class TextDigestError extends Error {}
-
 export async function sha256(text: string) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore - Support in Safari before version 11 was using the crypto.webkitSubtle prefix.
     if (!window.crypto.subtle && !window.crypto.webkitSubtle) {
-        return sha256_cryptojs(text).toString();
-        // throw new TextDigestError("浏览器版本过低或没有运行在HTTPS环境");
+        // TODO: use crypto-js to digest
+        const sha256 = (await import("crypto-js/sha256")).default;
+        return sha256(text).toString();
     } else {
         const digestedBuffer = await window.crypto.subtle.digest(
             "SHA-256",
