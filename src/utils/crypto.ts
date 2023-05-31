aad9d99dd922ef0b4afa2dad7d031853cfa0ab1f
SeraphJACK <seraphjack@outlook.com>
2023-05-31 23:05:06 +0800

    Fallback crypto to crypto-js

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,11 +1,13 @@
+import sha256_cryptojs from "crypto-js/sha256";
+
 class TextDigestError extends Error {}
 
 export async function sha256(text: string) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore - Support in Safari before version 11 was using the crypto.webkitSubtle prefix.
     if (!window.crypto.subtle && !window.crypto.webkitSubtle) {
-        // TODO: use crypto-js to digest
-        throw new TextDigestError("浏览器版本过低或没有运行在HTTPS环境");
+        return sha256_cryptojs(text).toString();
+        // throw new TextDigestError("浏览器版本过低或没有运行在HTTPS环境");
     } else {
         const digestedBuffer = await window.crypto.subtle.digest(
             "SHA-256",
