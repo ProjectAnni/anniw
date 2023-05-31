cf3aef802d236abfdabadf389e2f295ed0c6a218
Eridanus Sora <sora@sound.moe>
2023-05-31 23:10:57 +0800

    remove todo

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -2,7 +2,6 @@ export async function sha256(text: string) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore - Support in Safari before version 11 was using the crypto.webkitSubtle prefix.
     if (!window.crypto.subtle && !window.crypto.webkitSubtle) {
-        // TODO: use crypto-js to digest
         const sha256 = (await import("crypto-js/sha256")).default;
         return sha256(text).toString();
     } else {
