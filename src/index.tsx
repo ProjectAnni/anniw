c0193ec9c557fb1f76f672162f0f6bf4fb5f8357
Eridanus Sora <sora@sound.moe>
2023-06-01 19:53:23 +0800

    disable service worker features in non-secure context

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./global.css";
 
-navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    for (const registration of registrations) {
-        registration.unregister();
-    }
-});
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (const registration of registrations) {
+            registration.unregister();
+        }
+    });
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
